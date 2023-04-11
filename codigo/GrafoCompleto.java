e095ca5faf395696e5627c1a6a5f9a2979b6155c
Klaus8906 <107149823+Klaus8906@users.noreply.github.com>
2023-04-11 17:22:45 -0300

    Update GrafoCompleto.java

--- codigo/GrafoCompleto.java
+++ codigo/GrafoCompleto.java
@@ -1,25 +1,6 @@
 public class GrafoCompleto extends Grafo {
   
-    private int ordem;
-    
-//    public GrafoCompleto(String nome, int ordem) {
-//        super(nome);
-//        this.setOrdem(ordem);
-//
-//        this.vertices = new ABB<>();
-//        Vertice[] arrayVertices = new Vertice[ordem];
-//
-//        int i = 0;
-//        for (Vertice vertice : arrayVertices) {
-//            vertice = this.addVertice(i++);
-//
-//            for (int j = 0; j < (i - 1); j++) {
-//                this.addAresta(j, vertice.getId(), 0);
-//                this.addAresta(vertice.getId(), j, 0); 
-//            }
-//        }
-//    }
-//    
+    private int ordem;   
     
     public GrafoCompleto(String nome, int ordem) {
         super(nome);
